c0716922a08ef81976216394b21f55f477313417
LYSANDROS Dimitrios <Dimitrios.LYSANDROS@akka.eu>
2018-12-31 10:20:33 +0100

    End of section 4

--- TeamUp.API/Controllers/ValuesController.cs
+++ TeamUp.API/Controllers/ValuesController.cs
@@ -23,6 +23,7 @@ namespace TeamUp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
