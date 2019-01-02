35dd6ddf3694492568b3a987c93836a3cf3ac559
LYSANDROS Dimitrios <Dimitrios.LYSANDROS@akka.eu>
2019-01-02 14:30:22 +0100

    End of section 6

--- TeamUp-SPA/src/app/register/register.component.ts
+++ TeamUp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   model: any = {};
 
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -21,15 +22,15 @@ export class RegisterComponent implements OnInit {
 
     this.authService.register(this.model)
       .subscribe(() => {
-        console.log('registeration successful');
+        this.alertify.success('Registeration successfull!');
       }, error => {
-        console.log(error);
+        this.alertify.error(error);
       });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Registeration was canceled');
+    this.alertify.message('Registeration was canceled');
   }
 
 }
